d34d36f21eadcd20400c6a68c4439038fccd6ff1
Fernando Carvajal <efcu93@gmail.com>
2018-11-12 18:54:51 -0500

    cambios estaticos

--- app.js
+++ app.js
@@ -28,11 +28,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 
 app.use(express.static(`${__dirname}/public`, {
-  setHeaders: res => res.setHeader('Cache-Control', 'private, max-age=3600') //60*60
+    setHeaders: res => {
+        res.setHeader('Cache-Control', 'private, max-age=3600') //60*60
+        res.setHeader('X-Desarrollado-Por', 'Edgar Carvajal efcu93@gmail.com')
+    }
 }))
+
 app.use((req, res, next) => {
-  res.set('X-Desarrollado-Por', 'Edgar Carvajal efcu93@gmail.com');
-  next()
+    res.set('X-Desarrollado-Por', 'Edgar Carvajal efcu93@gmail.com');
+    next()
 })
 
 app.use('/', require('./routes/index'));
@@ -43,18 +47,18 @@ app.use('/', require('./routes/packin'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
--- public/service-worker.js
+++ public/service-worker.js
@@ -7,78 +7,11 @@
  * Este SW servira para incrementar el performance y reducir el trafico de los aplicativos usados en ITSC
  */
 
-const CACHE_NAME = 'Static-ITSCwebapps-v1.2'
-
-const audiograma = [
-    '/audiogramaITSC/index.html',
-    '/audiogramaITSC/',
-    '/audiogramaITSC/media/css/style.css',
-    '/audiogramaITSC/media/img/audiogram/left.air.masked.response.png',
-    '/audiogramaITSC/media/img/audiogram/left.air.unmasked.response.png',
-    '/audiogramaITSC/media/img/audiogram/left.bone.masked.response.png',
-    '/audiogramaITSC/media/img/audiogram/left.bone.unmasked.response.png',
-    '/audiogramaITSC/media/img/audiogram/right.air.masked.response.png',
-    '/audiogramaITSC/media/img/audiogram/right.air.unmasked.response.png',
-    '/audiogramaITSC/media/img/audiogram/right.bone.masked.response.png',
-    '/audiogramaITSC/media/img/audiogram/right.bone.unmasked.response.png',
-    '/audiogramaITSC/media/img/audiogram/soundfield.aided.response.png',
-    '/audiogramaITSC/media/img/audiogram/soundfield.ci.response.png',
-    '/audiogramaITSC/media/img/audiogram/soundfield.unaided.response.png',
-    '/audiogramaITSC/media/js/audiograma.js',
-    '/audiogramaITSC/media/js/ba-debug.min.js',
-    '/audiogramaITSC/media/js/jquery.audiogram.js',
-    '/audiogramaITSC/media/js/jquery.js'
-]
-
-const calendario = [
-    '/calendarioAPP/index.html',
-    '/calendarioAPP/',
-    '/calendarioAPP/lib/jquery-ui.min.js',
-    '/calendarioAPP/lib/jquery.min.js',
-    '/calendarioAPP/lib/moment.min.js',
-    '/calendarioAPP/locale/es.js',
-    '/calendarioAPP/fullcalendar.css',
-    '/calendarioAPP/fullcalendar.js',
-    '/calendarioAPP/fullcalendar.min.css',
-    '/calendarioAPP/fullcalendar.min.js',
-    '/calendarioAPP/fullcalendar.print.css',
-    '/calendarioAPP/fullcalendar.print.min.css',
-    '/calendarioAPP/jquery.qtip.css',
-    '/calendarioAPP/jquery.qtip.js',
-    '/calendarioAPP/jquery.qtip.min.css',
-    '/calendarioAPP/jquery.qtip.min.js',
-    '/calendarioAPP/jquery.qtip.min.map'
-]
-
-const qr = [
-    '/qrGEN/index.html',
-    '/qrGEN/jquery.min.js',
-    '/qrGEN/jquery.qrcode.js',
-    '/qrGEN/qrcode.js',
-    '/qrGEN/'
-]
-
-const tablaGenerica = [
-    '/tablaGenerica/index.html',
-    '/tablaGenerica/',
-    '/tablaGenerica/buttons.dataTables.min.css',
-    '/tablaGenerica/buttons.flash.min.js',
-    '/tablaGenerica/buttons.html5.min.js',
-    '/tablaGenerica/dataTables.buttons.min.js',
-    '/tablaGenerica/dataTables.pageResize.min.js',
-    '/tablaGenerica/jquery.dataTables.min.css',
-    '/tablaGenerica/jquery.dataTables.min.js',
-    '/tablaGenerica/jquery.js',
-    '/tablaGenerica/jszip.min.js',
-    '/tablaGenerica/images/sort_asc.png',
-    '/tablaGenerica/images/sort_both.png',
-    '/tablaGenerica/images/sort_desc.png'
-]
+const CACHE_NAME = 'Static-ITSCwebapps-v1.3'
 
 self.addEventListener('install', function (event) {
     event.waitUntil(async function() {
         const cache = await caches.open(CACHE_NAME);
-        //await cache.addAll([...audiograma, ...calendario, ...qr, ...tablaGenerica])
         await cache.addAll(['/audiogramaITSC/', '/tablaGenerica/', '/qrGEN/', '/calendarioAPP/'])
     }())
 })
