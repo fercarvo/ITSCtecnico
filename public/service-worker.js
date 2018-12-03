4769fbff78ca46e72da7f82507e4ffbddce396d6
Fernando Carvajal <efcu93@gmail.com>
2018-12-03 18:53:51 -0500

    push noti

--- public/service-worker.js
+++ public/service-worker.js
@@ -27,6 +27,15 @@ self.addEventListener('activate', function(event) {
     }())    
 })
 
+self.addEventListener('push', function (event) {
+    const data = event.data.json();
+    console.log('Got push', data);
+    self.registration.showNotification(data.title, {
+        body: 'Hello, World!',
+        icon: 'http://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png'
+    });
+})
+
 self.addEventListener('fetch', function(event) {
     const not_cache = [
         '/login',
---
+++ routes/push.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var login = require('./login').router
+
+const webpush = require('web-push');
+const { PUBLIC_VAPID_KEY, PRIVATE_VAPID_KEY } = require('../util/DB.js');
+
+// Replace with your email
+webpush.setVapidDetails('mailto:efcu93@gmail.com', PUBLIC_VAPID_KEY, PRIVATE_VAPID_KEY);
+
+router.post('/subscribe', login.validarSesion, (req, res, next) => {
+    const subscription = req.body;
+    res.status(201).json({});
+    const payload = JSON.stringify({ title: 'test' });
+  
+    console.log(subscription);
+  
+    webpush.sendNotification(subscription, payload).catch(error => {
+        console.error(error.stack);
+    });
+});
+
+module.exports = router;
