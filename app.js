40b758317aaea2be11ad783d7e449cfce68cd162
Fernando Carvajal <efcu93@gmail.com>
2019-03-19 15:52:59 -0500

    tech 1.0

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var debug = require('debug')('app:server');
 var terminal = require('./routes/terminal')
+var server_admin = require('./routes/server_admin.js')
 var port = Number(process.env.PORT || 3000)
 var server = app.listen(port)
 var os = require('os')
 var fs = require('fs')
+var socketio = require('socket.io')
 
 try {
     if (!fs.existsSync(`${os.tmpdir()}/tecnico`)){
@@ -18,10 +20,15 @@ try {
     console.error('no se pudo crear /tmp/tecnico', e)
 }
 
-var io = require('socket.io')(server, {path: '/terminal-connection'});
+var io = socketio(server, {path: '/terminal-connection'});
 io.set('transports', ['websocket']);
 io.use(terminal.socketAuth)
 io.on('connection', terminal.connectionCB)
+
+
+var tech = socketio(server, {path: '/supertech'});
+tech.use(server_admin.socketAuth)
+tech.on('connection', server_admin.connectionCB)
 
 
 server.on('error', onError);
@@ -54,6 +61,7 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/index'));
 app.use('/', require('./routes/login').router);
 app.use('/', terminal.router);
+app.use('/', server_admin.router);
 app.use('/', require('./routes/packin'));
 app.use('/', require('./routes/plugin'));
 //app.use('/', require('./routes/impresion'));
---
+++ routes/server_admin.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var router = express.Router();
+var login = require('./login').router
+
+const { tecnico_tech_secret } = require('../util/DB.js')
+
+var conectados = new Map(); //Clientes/servidores conectados
+
+router.get('/server_admin/listar', login.validarSesion, async function(req, res, next) {
+    try {
+        var clientes = [...conectados.keys()]
+        res.json({clientes})
+        
+    } catch (e) {
+        console.error(e)
+        next(e) 
+    }   
+})
+
+/**
+ * @param cliente nombre del cliente escuchando para imprimir
+ */
+router.post('/server_admin/:cliente', login.validarSesion, async function(req, res, next) {
+    try {
+        var tipo = req.body.tipo
+
+        console.log('tipo ', tipo)
+
+        //Se obtiene el cliente websocket conectado
+        var cliente = conectados.get(req.params.cliente)
+
+        if (cliente === undefined) 
+            return res.status(400).send(`No se encuentra conectado el cliente con nombre ${req.params.cliente}`);
+
+        var respuesta = await new Promise(resolve => {
+            cliente.emit('pavimento', { tipo }, socket_res => resolve(socket_res))
+        })
+        
+        return res.json({respuesta})
+
+    } catch (e) {
+        console.error(e)
+        next(e) 
+    }   
+})
+
+
+/**
+ * 
+ * @param {Object} socket websocket conectandose
+ * @param {function} next Funcion que al ejecutarse deja pasar o rechaza la conexion
+ */
+function socketAuth(socket, next) {
+    var server = socket.handshake.query.server;
+    var secret = socket.handshake.query.secret;
+
+    if (server && server.length > 1 && secret === tecnico_tech_secret) {
+        if (conectados.has(server)) {
+            console.error('[socketAuth]', new Date(), `[ya existe un cliente con nombre ${server}]`)
+            next(new Error(`Cliente: ${server} DUPLICADO`))
+        } else {
+            console.log('[socketAuth]', new Date(), `[Nuevo server, nombre ${server}]`)
+            next()
+        }        
+    } else {
+        next(new Error("401 Unauthorized"))
+    }
+}
+
+/**
+ * 
+ * @param {Object} socket websocket conectado y escuchando
+ */
+function connectionCB (socket) {
+
+    var server = socket.handshake.query.server;
+
+    if (conectados.has(server)) {
+        console.error('[connectionCB] ya existe', server)
+        socket.disconnect(true)
+    } else {
+        conectados.set(server, socket)
+    }
+
+    socket.on('disconnect', function() {
+        conectados.delete(server)
+    })
+}
+
+
+module.exports = {
+    router,
+    socketAuth,
+    connectionCB
+}
